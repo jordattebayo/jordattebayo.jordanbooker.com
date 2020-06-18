3a753989cb575ef22235f6f17cab9eb87bc2306d
Jordan Booker <jordan.m.booker@gmail.com>
2020-06-18 00:35:17 -0400

    Created global style sheet and Layout css module

--- jordattebayo/components/Navbar.js
+++ jordattebayo/components/Navbar.js
@@ -1,23 +1,31 @@
 import Link from "next/link";
 
 const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
-      <li>
-        <a href="https://www.github.com/jordattebayo">Github</a>
-      </li>
-    </ul>
-  </nav>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <a href="https://www.github.com/jordattebayo">Github</a>
+        </li>
+      </ul>
+    </nav>
+    <style jsx>{`
+      html,
+      a {
+        text-decoration: none;
+      }
+    `}</style>
+  </div>
 );
 
 export default Navbar;
