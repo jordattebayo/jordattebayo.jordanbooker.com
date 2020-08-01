07e1252a04c24607524433cd588cf891288bc4e6
Jordan Booker <jordan.m.booker@gmail.com>
2020-08-01 02:38:58 -0400

    Project Card building and general styling
    Added project card JSX and styling. Fixed CSS for the project holder.

--- jordattebayo/components/ProjectCard.js
+++ jordattebayo/components/ProjectCard.js
@@ -2,12 +2,23 @@ import styles from "./projectCard.module.css";
 
 export default function ProjectCard() {
   return (
-    <div className={styles.pContainer}>
-      <p className={styles.pText}>Project 1</p>
-      <p>
-        I am constantly adding things so feel free to come back and check on
-        things. Hopefully you'll come back and check it out!
-      </p>
+    <div className={styles.container}>
+      <h3 className={styles.titleText}>Project Name</h3>
+      <div className={styles.info}>
+        <a className={styles.imgLink}>
+          <img
+            className={styles.proImg}
+            src="https://cdn.pixabay.com/photo/2020/06/18/07/56/railing-5312344_960_720.jpg"
+          />
+        </a>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>Role: Something</p>
+          <p className={styles.text}>Project Difficulties: Something</p>
+          <p className={styles.text}>My Solution: Something</p>
+          <p className={styles.text}>Notable Features: Something</p>
+          <p className={styles.text}>Technologies Used: HTML, CSS, Boostrap</p>
+        </div>
+      </div>
     </div>
   );
 }
